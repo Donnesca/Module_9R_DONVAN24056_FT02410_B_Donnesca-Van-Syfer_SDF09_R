a5033603aef7755f610fa89260584c23747c252a
Donnesca <163658888+Donnesca@users.noreply.github.com>
2024-11-19 20:20:32 +0200

    completed project

--- index.js
+++ index.js
@@ -3,22 +3,39 @@
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
+let player = {
+  name: "Donnesca",
+  chips: 200,
+};
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isalive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+
+playerEl.textContent = player.name + ": $" + player.chips;
 
 function getRandomCard() {
-  return 5;
+  let randomNumber = Math.floor(Math.random() * 13) + 1;
+  if (randomNumber > 10) {
+    return 10;
+  } else if (randomNumber === 1) {
+    return 11;
+  } else {
+    return randomNumber;
+  }
 }
 
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   renderGame();
 }
 
@@ -36,15 +53,16 @@ function renderGame() {
     hasBlackJack = true;
   } else {
     message = "You're out of the game!";
-    isalive = false;
+    isAlive = false;
   }
   messageEl.textContent = message;
 }
 
 function newCard() {
-  let card = getRandomCard();
-  sum += card;
-  cards.push(card);
-  console.log(cards);
-  renderGame();
+  if (isAlive === true && hasBlackJack === false) {
+    let card = getRandomCard();
+    sum += card;
+    cards.push(card);
+    renderGame();
+  }
 }
