c877caf22fb04b1c70f9e63265d6cc7dfcaf5469
Donnesca <163658888+Donnesca@users.noreply.github.com>
2024-11-19 17:07:43 +0200

    Halfway through my project, addedindex.js and index.css. added background, buttons, changed font,added majority of the js functions as well.

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-// 1. Create two variables, firstCard and secondCard. 
+// 1. Create two variables, firstCard and secondCard.
 // Set their values to a random number between 2-11
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 6
-let secondCard = 9
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isalive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-let sum = firstCard + secondCard
+function getRandomCard() {
+  return 5;
+}
 
-console.log(sum)
+function startGame() {
+  renderGame();
+}
+
+function renderGame() {
+  cardsEl.textContent = "Cards: ";
+  for (let i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
+  sumEl.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isalive = false;
+  }
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  let card = getRandomCard();
+  sum += card;
+  cards.push(card);
+  console.log(cards);
+  renderGame();
+}
